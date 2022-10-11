04c0e7d497c7b851539a20901bc9286babdce834
hyper_puncher <imolohovskiy@gmail.com>
2022-10-11 18:16:14 +0300

    scrippity sctipt

---
+++ install_2.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+sudo dnf install --allowerasing -y i3-gaps kitty code arandr feh unzip lxpolkit rofi picom nnn lxappearance easyeffects xprop neofetch polybar telegram zsh playerctl ddccontrol ddccontrol-gtk zsh-autosuggestions zsh-syntax-highlighting redshift xclip xdotool materia-gtk-theme papirus-icon-theme exa mpv qbittorrent android-file-transfer &&
+
+echo "Poopity poop may I offer reboop"
+
