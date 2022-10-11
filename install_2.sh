9efc382f4855dba9b49b07625cf1ac3beed11c84
hyper_puncher <imolohovskiy@gmail.com>
2022-10-11 23:13:31 +0300

    upd

--- install_2.sh
+++ install_2.sh
@@ -2,11 +2,11 @@
 
 sudo dnf upgrade -y &&
 
-dnf install --allowerasing -y i3-gaps kitty code arandr feh unzip lxpolkit rofi picom nnn lxappearance easyeffects xprop neofetch polybar telegram zsh playerctl ddccontrol ddccontrol-gtk zsh-autosuggestions zsh-syntax-highlighting redshift xclip xdotool materia-gtk-theme papirus-icon-theme exa mpv qbittorrent android-file-transfer &&
+sudo dnf install --allowerasing -y i3-gaps kitty code arandr feh unzip lxpolkit rofi picom nnn lxappearance easyeffects xprop neofetch polybar telegram zsh playerctl ddccontrol ddccontrol-gtk zsh-autosuggestions zsh-syntax-highlighting redshift xclip xdotool materia-gtk-theme papirus-icon-theme exa mpv qbittorrent android-file-transfer telegram-desktop &&
 
 cd dotfiles &&
 
 stow -t /home/igor/ */ &&
 
-echo "Poopity poop may I offer reboop"
+echo  -e "\e[1;36mPoopity poop may I offer reboop"
 
