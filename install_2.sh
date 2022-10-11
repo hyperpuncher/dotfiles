77451a553c9c0e058f9601b14763efb554c67362
hyper_puncher <imolohovskiy@gmail.com>
2022-10-11 18:40:26 +0300

    upd

--- install_2.sh
+++ install_2.sh
@@ -2,5 +2,9 @@
 
 sudo dnf install --allowerasing -y i3-gaps kitty code arandr feh unzip lxpolkit rofi picom nnn lxappearance easyeffects xprop neofetch polybar telegram zsh playerctl ddccontrol ddccontrol-gtk zsh-autosuggestions zsh-syntax-highlighting redshift xclip xdotool materia-gtk-theme papirus-icon-theme exa mpv qbittorrent android-file-transfer &&
 
+cd dotfiles &&
+
+stow -t /home/igor/ */ &&
+
 echo "Poopity poop may I offer reboop"
 
