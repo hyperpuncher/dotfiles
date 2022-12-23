3ec1eba6f44be588d46711ef2fda62e245e66482
hyper_puncher <39203616+hyperpuncher@users.noreply.github.com>
2022-12-23 10:33:40 +0300

    upd

--- install_2.sh
+++ install_2.sh
@@ -2,7 +2,7 @@
 
 sudo dnf upgrade -y &&
 
-sudo dnf install --allowerasing -y i3-gaps kitty code arandr feh unzip lxpolkit rofi picom nnn lxappearance easyeffects xprop neofetch polybar telegram zsh playerctl ddccontrol ddccontrol-gtk zsh-autosuggestions zsh-syntax-highlighting redshift xclip xdotool materia-gtk-theme papirus-icon-theme exa mpv qbittorrent android-file-transfer android-tools telegram-desktop speedcrunch btop ncdu fzf ImageMagick blueman kitty &&
+sudo dnf install --allowerasing -y i3-gaps kitty code arandr feh unzip lxpolkit rofi picom nnn lxappearance easyeffects xprop neofetch polybar telegram zsh playerctl ddccontrol ddccontrol-gtk zsh-autosuggestions zsh-syntax-highlighting redshift xclip xdotool materia-gtk-theme papirus-icon-theme exa mpv qbittorrent android-file-transfer android-tools telegram-desktop speedcrunch btop ncdu fzf ImageMagick blueman krita handbrake-gui &&
 
 cd dotfiles &&
 
