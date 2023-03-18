4596384564aeec7ab7ddc29e999b51df1b3ea8c0
hyperpuncher <imolohovskiy@gmail.com>
2023-03-18 08:02:43 +0300

    upd

--- waybar/.config/waybar/scripts/get_spotify.sh
+++ waybar/.config/waybar/scripts/get_spotify.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
-media=$(playerctl metadata -f "{{artist}} - {{title}}")
 player_status=$(playerctl status)
 
-if [[ $player_status = "Playing" ]]
+if [[ $player_status = "" ]]
 then
-    song_status=''
-elif [[ $player_status = "Paused" ]]
-then
-    song_status=''
-else
     song_status='Spotify '
+else
+    media=$(playerctl metadata -f "{{artist}} - {{title}}")
+    if [[ $player_status = "Playing" ]]
+    then
+        song_status=''
+    else
+        song_status=''
+    fi
+    echo -e "$song_status $media"
 fi
-
-echo -e "$song_status $media"
