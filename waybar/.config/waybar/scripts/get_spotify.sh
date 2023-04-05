0a84ea4907eee7971f217007e318dd2a3978f785
hyperpuncher <imolohovskiy@gmail.com>
2023-04-05 12:55:39 +0300

    upd

--- waybar/.config/waybar/scripts/get_spotify.sh
+++ waybar/.config/waybar/scripts/get_spotify.sh
@@ -6,6 +6,14 @@ then
     song_status='Spotify '
 else
     media=$(playerctl metadata -f "{{artist}} - {{title}}")
+    media_chars=$(echo $media | wc -c)
+
+    if [[ $media_chars -gt 32 ]]
+    then
+        media=$(echo "$media" | cut -c -32)
+        media+="..."
+    fi
+
     if [[ $player_status = "Playing" ]]
     then
         song_status=''
