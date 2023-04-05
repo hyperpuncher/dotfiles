26616d8f9953565eec43e9e39585ada1c641e394
hyperpuncher <imolohovskiy@gmail.com>
2023-04-05 09:59:14 +0300

    firefox

---
+++ firefox/user.js
@@ -0,0 +1,14 @@
+user_pref("browser.aboutConfig.showWarning", false);
+user_pref("browser.fullscreen.autohide", false);
+user_pref("full-screen-api.warning.delay", 0);
+user_pref("full-screen-api.warning.timeout", 0);
+user_pref("extensions.pocket.enabled", false);
+user_pref("browser.compactmode.show", true);
+user_pref("browser.uidensity", 1);
+user_pref("browser.tabs.inTitlebar", 1);
+user_pref("media.videocontrols.picture-in-picture.video-toggle.enabled", false);
+user_pref("browser.newtabpage.activity-stream.showSponsoredTopSites", false);
+user_pref("media.hardwaremediakeys.enabled", false);
+user_pref("services.sync.declinedEngines", "creditcards,passwords");
+user_pref("services.sync.engine.passwords", false);
+user_pref("browser.startup.homepage", "file:///home/igor/projects/startpage/index.html");
