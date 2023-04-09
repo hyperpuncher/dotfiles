c799d8762ca67c007c627a38f34497bb88972028
hyperpuncher <imolohovskiy@gmail.com>
2023-04-09 07:45:07 +0300

    upd

--- waybar/.config/waybar/scripts/get_spotify.sh
+++ waybar/.config/waybar/scripts/get_spotify.sh
@@ -1,21 +1,18 @@
 #!/bin/bash
 player_status=$(playerctl status)
 
-if [[ $player_status = "" ]]
-then
+if [[ $player_status = "" ]]; then
     song_status='Spotify '
 else
     media=$(playerctl metadata -f "{{artist}} - {{title}}")
-    media_chars=$(echo $media | wc -c)
+    media_chars=$(echo "$media" | wc -c)
 
-    if [[ $media_chars -gt 32 ]]
-    then
+    if [[ $media_chars -gt 32 ]]; then
         media=$(echo "$media" | cut -c -32)
         media+="..."
     fi
 
-    if [[ $player_status = "Playing" ]]
-    then
+    if [[ $player_status = "Playing" ]]; then
         song_status=''
     else
         song_status=''
