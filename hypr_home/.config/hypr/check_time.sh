7c65725106bd6fbebe53a53cf3abe803cb3b3e47
hyperpuncher <imolohovskiy@gmail.com>
2023-05-13 08:56:36 +0300

    upd

--- hypr_home/.config/hypr/check_time.sh
+++ hypr_home/.config/hypr/check_time.sh
@@ -1,25 +1,24 @@
 #!/bin/bash
 
-wakeup=$(( 6 * 60 ))
-sunset=$(( 19 * 60 ))
-night=$(( 22 * 60 ))
+wakeup=$((6 * 60))
+sunset=$((20 * 60))
+night=$((22 * 60))
 
 color_day=65
 color_night=25
 
 while true; do
-
     current_hour=$(date +%k)
     current_minutes=$(date +%M)
-    current_time=$(( current_hour * 60 + current_minutes ))
+    current_time=$((current_hour * 60 + current_minutes))
 
-    if (( current_time >= wakeup && current_time < sunset )); then
+    if ((current_time >= wakeup && current_time < sunset)); then
         color_temp=$color_day
 
-    elif (( current_time >= sunset && current_time < night )); then
-        range=$(( night - sunset ))
-        percent=$(echo "($current_time - $sunset) / $range" | bc -l )
-        color_temp=$(echo "$color_day - (($color_day - $color_night) * $percent)" | bc -l )
+    elif ((current_time >= sunset && current_time < night)); then
+        range=$((night - sunset))
+        percent=$(echo "($current_time - $sunset) / $range" | bc -l)
+        color_temp=$(echo "$color_day - (($color_day - $color_night) * $percent)" | bc -l)
         color_temp=$(printf "%.0f" "$color_temp")
 
     else
@@ -27,7 +26,6 @@ while true; do
     fi
 
     sed -i "s/temperature = [0-9][0-9]/temperature = $color_temp/" ~/dotfiles/hypr_home/.config/hypr/night_light.glsl
-    # echo "$color_temp"
 
     sleep 90
 done
