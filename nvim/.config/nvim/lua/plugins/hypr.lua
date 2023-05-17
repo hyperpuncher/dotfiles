d21bb3687600f7fc358232b2bc118ad085bd27ae
hyperpuncher <39203616+hyperpuncher@users.noreply.github.com>
2023-05-17 19:41:34 +0300

    upd

--- nvim/.config/nvim/lua/plugins/hypr.lua
+++ nvim/.config/nvim/lua/plugins/hypr.lua
@@ -8,5 +8,23 @@ return {
     },
   },
 
-  { "Exafunction/codeium.vim" },
+  {
+    "Exafunction/codeium.vim",
+    config = function()
+      vim.keymap.set("i", "<C-g>", function()
+        return vim.fn["codeium#Accept"]()
+      end, { expr = true })
+    end,
+  },
+
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    opts = {
+      filesystem = {
+        filtered_items = {
+          hide_dotfiles = false,
+        },
+      },
+    },
+  },
 }
