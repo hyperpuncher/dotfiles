ca85f65acd7ab75602c16a85d5c2c558b41dd118
hyperpuncher <imolohovskiy@gmail.com>
2023-05-19 12:07:56 +0300

    upd

--- nvim/.config/nvim/lua/plugins/hypr.lua
+++ nvim/.config/nvim/lua/plugins/hypr.lua
@@ -57,7 +57,7 @@ return {
   {
     "Exafunction/codeium.vim",
     config = function()
-      vim.keymap.set("i", "<C-g>", function()
+      vim.keymap.set("i", "<C-l>", function()
         return vim.fn["codeium#Accept"]()
       end, { expr = true })
     end,
