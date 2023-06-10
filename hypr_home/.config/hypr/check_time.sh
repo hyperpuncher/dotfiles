5e9f4222e571b115b3892d78235e457714b54bc4
hyperpuncher <imolohovskiy@gmail.com>
2023-06-10 08:46:33 +0300

    upd

--- hypr_home/.config/hypr/check_time.sh
+++ hypr_home/.config/hypr/check_time.sh
@@ -28,5 +28,5 @@ while true; do
     sed -i "s/temperature = [0-9][0-9]/temperature = $color_temp/" ~/dotfiles/hypr_home/.config/hypr/night_light.glsl
 
     echo "$color_temp"
-    sleep 60
+    sleep 30
 done
