78d9965a212f5052a0723518465658541f4d348e
hyperpuncher <imolohovskiy@gmail.com>
2023-06-10 09:42:32 +0300

    upd

--- nvim/.config/nvim/lua/plugins/hypr.lua
+++ nvim/.config/nvim/lua/plugins/hypr.lua
@@ -12,6 +12,11 @@ return {
     opts = {
       servers = {
         pyright = {
+          capabilities = (function()
+            local capabilities = vim.lsp.protocol.make_client_capabilities()
+            capabilities.textDocument.publishDiagnostics.tagSupport.valueSet = { 2 }
+            return capabilities
+          end)(),
           settings = {
             python = {
               analysis = {
@@ -43,7 +48,6 @@ return {
       ensure_installed = {
         "black",
         "ruff",
-        "ruff-lsp",
         "rustywind",
         "prettier",
         "prettierd",
