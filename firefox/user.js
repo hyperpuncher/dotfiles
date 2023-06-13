bcfda8eb3622ee867b02003401a321f9875439bc
hyperpuncher <39203616+hyperpuncher@users.noreply.github.com>
2023-06-13 18:48:34 +0300

    betterfox

--- firefox/user.js
+++ firefox/user.js
@@ -1,17 +1,31 @@
+// Smooth Scroll 120hz
+user_pref("general.smoothScroll.msdPhysics.continuousMotionMaxDeltaMS", 12);
+user_pref("general.smoothScroll.msdPhysics.enabled", true);
+user_pref("general.smoothScroll.msdPhysics.motionBeginSpringConstant", 600);
+user_pref("general.smoothScroll.msdPhysics.regularSpringConstant", 650);
+user_pref("general.smoothScroll.msdPhysics.slowdownMinDeltaMS", 25);
+user_pref("general.smoothScroll.msdPhysics.slowdownMinDeltaRatio", 2.0);
+user_pref("general.smoothScroll.msdPhysics.slowdownSpringConstant", 250);
+user_pref("general.smoothScroll.currentVelocityWeighting", 1.0);
+user_pref("general.smoothScroll.stopDecelerationWeighting", 1.0);
+user_pref("mousewheel.default.delta_multiplier_y", 300); // 250-400
+
 user_pref("browser.aboutConfig.showWarning", false);
+user_pref("browser.download.always_ask_before_handling_new_types", false);
+user_pref("browser.download.alwaysOpenPanel", false);
+user_pref("browser.download.useDownloadDir", true);
 user_pref("browser.fullscreen.autohide", false);
-user_pref("full-screen-api.warning.delay", 0);
-user_pref("full-screen-api.warning.timeout", 0);
-user_pref("extensions.pocket.enabled", false);
-user_pref("browser.compactmode.show", true);
-user_pref("browser.uidensity", 1);
-user_pref("browser.tabs.inTitlebar", 1);
-user_pref("media.videocontrols.picture-in-picture.video-toggle.enabled", false);
 user_pref("browser.newtabpage.activity-stream.showSponsoredTopSites", false);
-user_pref("media.hardwaremediakeys.enabled", false);
-user_pref("services.sync.declinedEngines", "creditcards,passwords");
-user_pref("services.sync.engine.passwords", false);
+user_pref("browser.safebrowsing.downloads.remote.enabled", true);
 user_pref(
   "browser.startup.homepage",
   "file:///home/igor/projects/startpage/index.html"
 );
+user_pref("browser.tabs.inTitlebar", 1);
+user_pref("browser.uidensity", 1);
+user_pref("dom.push.enabled", true);
+user_pref("identity.fxaccounts.enabled", true);
+user_pref("media.hardwaremediakeys.enabled", false);
+user_pref("media.videocontrols.picture-in-picture.video-toggle.enabled", false);
+user_pref("services.sync.declinedEngines", "creditcards,passwords");
+user_pref("services.sync.engine.passwords", false);
