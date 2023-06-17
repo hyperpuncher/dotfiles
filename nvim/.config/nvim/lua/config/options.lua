cdec4f6161999feaa9c8c6064facce431261ef38
hyperpuncher <39203616+hyperpuncher@users.noreply.github.com>
2023-06-17 16:04:02 +0300

    new options

--- nvim/.config/nvim/lua/config/options.lua
+++ nvim/.config/nvim/lua/config/options.lua
@@ -1,10 +1,10 @@
--- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
--- Add any additional options here
 vim.opt.pumblend = 0
 vim.opt.pumheight = 6
 vim.opt.scrolloff = 10
+vim.opt.shiftwidth = 4
 vim.opt.spell = false
 vim.opt.swapfile = false
+vim.opt.tabstop = 4
 vim.opt.updatetime = 100
 vim.opt.wrap = true
