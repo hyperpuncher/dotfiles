19ff1dd8957ec73b59b4323699b65465277759bb
hyperpuncher <39203616+hyperpuncher@users.noreply.github.com>
2023-06-20 12:18:43 +0300

    wipe clipboard on shutdown

--- rofi/.config/rofi/scripts/power_menu.sh
+++ rofi/.config/rofi/scripts/power_menu.sh
@@ -3,13 +3,13 @@
 choice=$(printf "Shutdown\nReboot\nLogout" | rofi -dmenu -i -p "Power")
 
 case $choice in
-  "Shutdown")
-    systemctl poweroff
-    ;;
-  "Reboot")
-    systemctl reboot
-    ;;
-  "Logout")
-    hyprctl dispatch exit 0
-    ;;
+"Shutdown")
+	cliphist wipe && systemctl poweroff
+	;;
+"Reboot")
+	systemctl reboot
+	;;
+"Logout")
+	hyprctl dispatch exit 0
+	;;
 esac
