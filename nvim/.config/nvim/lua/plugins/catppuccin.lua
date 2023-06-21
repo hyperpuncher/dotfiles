4fffd45e253677794c2aa1b31bca5d43d0f1ea73
hyperpuncher <39203616+hyperpuncher@users.noreply.github.com>
2023-06-21 14:04:47 +0300

    cleanup

--- nvim/.config/nvim/lua/plugins/catppuccin.lua
+++ nvim/.config/nvim/lua/plugins/catppuccin.lua
@@ -1,23 +1,23 @@
 return {
-  {
-    "catppuccin/nvim",
-    name = "catppuccin",
-    opts = {
-      flavour = "mocha",
-      color_overrides = {
-        mocha = {
-          base = "#191919",
-          mantle = "#202020",
-          crust = "#262626",
-        }
-      },
+    {
+        "catppuccin/nvim",
+        name = "catppuccin",
+        opts = {
+            flavour = "mocha",
+            color_overrides = {
+                mocha = {
+                    base = "#191919",
+                    mantle = "#202020",
+                    crust = "#262626",
+                }
+            },
+        },
     },
-  },
 
-  {
-    "LazyVim/LazyVim",
-    opts = {
-      colorscheme = "catppuccin",
+    {
+        "LazyVim/LazyVim",
+        opts = {
+            colorscheme = "catppuccin",
+        },
     },
-  },
 }
