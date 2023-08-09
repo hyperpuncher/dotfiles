cc7f2516a346680630263df53be08d0bed8665cb
hyperpuncher <39203616+hyperpuncher@users.noreply.github.com>
2023-08-09 15:43:01 +0300

    cleanup

--- wireplumber/.config/wireplumber/main.lua.d/51-alsa-disable.lua
+++ wireplumber/.config/wireplumber/main.lua.d/51-alsa-disable.lua
@@ -1,13 +1,19 @@
 rule = {
     matches = {
+        -- display home
         {
             { "device.name", "equals", "alsa_card.pci-0000_07_00.1" },
         },
         {
             { "node.name", "equals", "alsa_output.pci-0000_09_00.4.iec958-stereo" },
         },
+        -- display work
         {
             { "device.name", "equals", "alsa_card.pci-0000_07_00.1.3" },
+        },
+        -- system card work
+        {
+            { "device.name", "equals", "alsa_card.pci-0000_07_00.6" },
         }
     },
     apply_properties = {
