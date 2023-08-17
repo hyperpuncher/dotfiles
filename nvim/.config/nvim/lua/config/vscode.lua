f5b7adeecd9e47ca9b72e64b7c3bb027c437067b
hyperpuncher <39203616+hyperpuncher@users.noreply.github.com>
2023-08-17 12:30:32 +0300

    change highlight color

--- nvim/.config/nvim/lua/config/vscode.lua
+++ nvim/.config/nvim/lua/config/vscode.lua
@@ -2,11 +2,13 @@ local function augroup(name)
     return vim.api.nvim_create_augroup("lazyvim_" .. name, { clear = true })
 end
 
+vim.cmd [[highlight MyHighlight guifg=black guibg=white]]
+
 -- Highlight on yank
 vim.api.nvim_create_autocmd("TextYankPost", {
     group = augroup("highlight_yank"),
     callback = function()
-        vim.highlight.on_yank()
+        vim.highlight.on_yank { higroup = "MyHighlight" }
     end,
 })
 
