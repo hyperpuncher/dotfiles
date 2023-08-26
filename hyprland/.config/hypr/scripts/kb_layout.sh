ebf0d38b9fd1305025add672630ac46989f281f5
hyperpuncher <39203616+hyperpuncher@users.noreply.github.com>
2023-08-26 12:06:18 +0300

    add variable to grab current keyboard device

--- hyprland/.config/hypr/scripts/kb_layout.sh
+++ hyprland/.config/hypr/scripts/kb_layout.sh
@@ -2,6 +2,8 @@
 active_window_class=""
 previous_window_class=""
 
+kb_name=$(hyprctl devices -j | jq -r '.keyboards[4].name')
+
 en_layout_apps="code-url-handler kitty firefox obsidian"
 ru_layout_apps="org.telegram.desktop"
 
@@ -10,12 +12,12 @@ while true; do
     if [ "$active_window_class" != "$previous_window_class" ]; then
         case " $en_layout_apps " in
         *" $active_window_class "*)
-            hyprctl switchxkblayout hyperpuncher-kanariyoi-keyboard 0
+            hyprctl switchxkblayout "$kb_name" 0
             ;;
         esac
         case " $ru_layout_apps " in
         *" $active_window_class "*)
-            hyprctl switchxkblayout hyperpuncher-kanariyoi-keyboard 1
+            hyprctl switchxkblayout "$kb_name" 1
             ;;
         esac
         previous_window_class="$active_window_class"
