90714fcb22c5e8614ff982cffad447979c29e285
hyperpuncher <39203616+hyperpuncher@users.noreply.github.com>
2023-08-28 09:01:07 +0300

    refactor to use proper kb names

--- hyprland/.config/hypr/scripts/kb_layout.sh
+++ hyprland/.config/hypr/scripts/kb_layout.sh
@@ -1,23 +1,32 @@
-#!/bin/dash
+#!/bin/bash
 active_window_class=""
 previous_window_class=""
 
-kb_name=$(hyprctl devices -j | jq -r '.keyboards[4].name')
+kb_names="bastard-keyboards-charybdis-mini-(3x6)-pro-micro hyperpuncher-kanariyoi"
+
+active_keyboard=""
 
 en_layout_apps="code-url-handler kitty firefox obsidian"
 ru_layout_apps="org.telegram.desktop"
+
+for name in ${kb_names}; do
+    if hyprctl devices -j | jq -e '.keyboards[].name' | grep -q "$name"; then
+        active_keyboard="$name"
+        break
+    fi
+done
 
 while true; do
     active_window_class=$(hyprctl activewindow | grep "class" | awk '{print $2}')
     if [ "$active_window_class" != "$previous_window_class" ]; then
         case " $en_layout_apps " in
         *" $active_window_class "*)
-            hyprctl switchxkblayout "$kb_name" 0
+            hyprctl switchxkblayout "$active_keyboard" 0
             ;;
         esac
         case " $ru_layout_apps " in
         *" $active_window_class "*)
-            hyprctl switchxkblayout "$kb_name" 1
+            hyprctl switchxkblayout "$active_keyboard" 1
             ;;
         esac
         previous_window_class="$active_window_class"
