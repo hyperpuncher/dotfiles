703f843be55609b65bffa29cfa73c5813da6dd70
hyperpuncher <39203616+hyperpuncher@users.noreply.github.com>
2023-12-08 14:16:19 +0300

    improve betterfox script

--- firefox/betterfox.sh
+++ firefox/betterfox.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
-cp ~/dotfiles/firefox/betterfox.js ~/.mozilla/firefox/*.default-release/user.js
-cat ~/dotfiles/firefox/user.js >>~/.mozilla/firefox/*.default-release/user.js
+curl https://raw.githubusercontent.com/yokoffing/Betterfox/main/user.js -o betterfox.js &&
+    cp ~/dotfiles/firefox/betterfox.js ~/.mozilla/firefox/*.default-release/user.js &&
+    cat ~/dotfiles/firefox/user.js >>~/.mozilla/firefox/*.default-release/user.js
