bd885df9172dba7803f9d006b5fdab5116e5857d
hyperpuncher <39203616+hyperpuncher@users.noreply.github.com>
2023-12-13 09:23:25 +0300

    cleanup betterfox script

--- firefox/betterfox.sh
+++ firefox/betterfox.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
-curl https://raw.githubusercontent.com/yokoffing/Betterfox/main/user.js -o betterfox.js &&
-    cp ~/dotfiles/firefox/betterfox.js ~/.mozilla/firefox/*.default-release/user.js &&
+curl https://raw.githubusercontent.com/yokoffing/Betterfox/main/user.js -o ~/.mozilla/firefox/*.default-release/user.js &&
     cat ~/dotfiles/firefox/user.js >>~/.mozilla/firefox/*.default-release/user.js
