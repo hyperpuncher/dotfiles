86eabb303f381b74d555ea4e5aa32b179171265f
hyperpuncher <39203616+hyperpuncher@users.noreply.github.com>
2023-12-22 14:09:12 +0300

    upd nixos

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -1,4 +1,79 @@
-programs.hyprland.enable = true
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{ config, pkgs, ... }:
+
+{
+  nixpkgs.config.permittedInsecurePackages = [
+    "electron-25.9.0"
+  ];
+
+  imports =
+    [
+      # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+    ];
+
+  # Bootloader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  networking.hostName = "nixos"; # Define your hostname.
+  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+
+  # Configure network proxy if necessary
+  # networking.proxy.default = "http://user:password@proxy:port/";
+  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+
+  # Enable networking
+  networking.networkmanager.enable = true;
+
+  hardware.bluetooth.enable = true;
+  hardware.bluetooth.powerOnBoot = true;
+  services.blueman.enable = true;
+
+  # Set your time zone.
+  time.timeZone = "Europe/Minsk";
+
+  # Select internationalisation properties.
+  i18n.defaultLocale = "en_US.UTF-8";
+
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "en_IE.UTF-8";
+    LC_IDENTIFICATION = "en_IE.UTF-8";
+    LC_MEASUREMENT = "en_IE.UTF-8";
+    LC_MONETARY = "en_IE.UTF-8";
+    LC_NAME = "en_IE.UTF-8";
+    LC_NUMERIC = "en_IE.UTF-8";
+    LC_PAPER = "en_IE.UTF-8";
+    LC_TELEPHONE = "en_IE.UTF-8";
+    LC_TIME = "en_IE.UTF-8";
+  };
+
+  # Configure keymap in X11
+  # services.xserver = {
+  # layout = "us";
+  # xkbVariant = "";
+  # };
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.igor = {
+    isNormalUser = true;
+    description = "igor";
+    extraGroups = [ "networkmanager" "wheel" ];
+    packages = with pkgs; [ ];
+  };
+
+  # Allow unfree packages
+  nixpkgs.config.allowUnfree = true;
+
+  programs.neovim = {
+    enable = true;
+    defaultEditor = true;
+  };
+
+  programs.hyprland.enable = true;
 
   services.greetd = {
     enable = true;
@@ -10,140 +85,165 @@ programs.hyprland.enable = true
     };
   };
 
-environment.systemPackages = with pkgs; [
-    kitty
-    rofi-wayland
-    android-file-transfer
-    android-tools
-    aria
-    bat
-    bibata-cursors
-    btop
-    bun
-    cliphist
-    corectrl
-    dash
-    ddcutil
-    xdragon
-    dunst
-    eza
-    fd
-    ffmpeg
-    gnome.file-roller
-    firefox
-    fnm
-    font-manager
-    fx
-    fzf
-    gdu
-    gimp
-    glow
-    gnome.gnome-disk-utility
-    gnome.gnome-keyring
-    gparted
-    grim
-    gvfs
-    hyprpicker
-    imagemagick
-    inkscape
-    jamesdsp
-    jq
-    qt6Packages.qtstyleplugin-kvantum
-    lazydocker
-    lazygit
-    lf
-    libreoffice-fresh
-    losslesscut-bin
-    man
-    moar
-    mpv
-    mtpfs
-    mumble
-    nasc
-    networkmanagerapplet
-    nfs-utils
-    nodejs
-    nvtop
-    nwg-look
-    obs-studio
-    obsidian
-    ocrmypdf
-    papirus-folders
-    papirus-icon-theme
-    parallel
-    pavucontrol
-    pfetch-rs
-    nodePackages.pnpm
-    polkit_gnome
-    procs
-    pulsemixer
-    qemu
-    qrencode
-    qt6.qtimageformats
-    libsForQt5.qt5.qtwayland
-    libsForQt5.qt5ct
-    qt6.qtwayland
-    qt6Packages.qt6ct
-    quickemu
-    qview
-    ripgrep
-    rofi-calc
-    rustdesk
-    shell_gpt
-    slurp
-    solaar
-    solvespace
-    speedtest-go
-    spotify
-    sunshine
-    swaybg
-    telegram-desktop
-    xfce.thunar
-    xfce.thunar-archive-plugin
-    xfce.thunar-volman
-    tlrc
-    transmission_4
-    udiskie
-    ungoogled-chromium
-    unzip
-    upscayl
-    ventoy
-    vial
-    vscodium
-    waybar
-    wget
-    wireguard-tools
-    wl-clipboard
-    wl-screenrec
-    wtype
-    xorg.xhost
-    yarn
-    yt-dlp
-    zip
-    zoxide
-]
-
-fonts.packages = with pkgs; [
+  fonts.packages = with pkgs; [
     noto-fonts
-    noto-fonts-cjk   
+    noto-fonts-cjk
     noto-fonts-emoji
-    
-    (nerdfonts.override { fonts = [
+
+    (nerdfonts.override {
+      fonts = [
         "CascadiaCode"
         "JetBrainsMono"
         "UbuntuMono"
-    ]; })
-]
+      ];
+    })
+  ];
 
-hardware.bluetooth.enable = true;
-hardware.bluetooth.powerOnBoot = true;
-services.blueman.enable = true;
 
-virtualisation.docker.enable = true;
-users.users.igor.extraGroups = [ "docker" ];
-virtualisation.docker.rootless = {
-  enable = true;
-  setSocketVariable = true;
-};
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+  environment.systemPackages = with pkgs;
+    [
+      android-file-transfer
+      android-tools
+      aria
+      bat
+      bibata-cursors
+      btop
+      bun
+      cliphist
+      corectrl
+      dash
+      ddcutil
+      dunst
+      eza
+      fd
+      ffmpeg
+      firefox
+      fnm
+      font-manager
+      fx
+      fzf
+      gdu
+      gimp
+      git
+      glow
+      gnome.file-roller
+      gnome.gnome-disk-utility
+      gnome.gnome-keyring
+      gparted
+      grim
+      gvfs
+      hyprpicker
+      imagemagick
+      inkscape
+      jamesdsp
+      jq
+      kitty
+      lazydocker
+      lazygit
+      lf
+      libreoffice-fresh
+      libsForQt5.qt5.qtwayland
+      libsForQt5.qt5ct
+      losslesscut-bin
+      man
+      moar
+      mpv
+      mtpfs
+      mumble
+      nasc
+      networkmanagerapplet
+      nfs-utils
+      nodePackages.pnpm
+      nodejs
+      nvtop
+      nwg-look
+      obs-studio
+      obsidian
+      ocrmypdf
+      papirus-folders
+      papirus-icon-theme
+      parallel
+      pavucontrol
+      pfetch-rs
+      polkit_gnome
+      procs
+      pulsemixer
+      qemu
+      qrencode
+      qt6.qtimageformats
+      qt6.qtwayland
+      qt6Packages.qt6ct
+      qt6Packages.qtstyleplugin-kvantum
+      quickemu
+      qview
+      ripgrep
+      rofi-calc
+      rofi-wayland
+      rustdesk
+      shell_gpt
+      slurp
+      solaar
+      solvespace
+      speedtest-go
+      spotify
+      stow
+      sunshine
+      swaybg
+      telegram-desktop
+      tlrc
+      transmission_4
+      udiskie
+      ungoogled-chromium
+      unzip
+      upscayl
+      ventoy
+      vial
+      vscodium
+      waybar
+      wget
+      wireguard-tools
+      wl-clipboard
+      wl-screenrec
+      wtype
+      xdragon
+      xfce.thunar
+      xfce.thunar-archive-plugin
+      xfce.thunar-volman
+      xorg.xhost
+      yarn
+      yt-dlp
+      zip
+      zoxide
+    ];
 
-boot.extraModulePackages = [ config.boot.kernelPackages.exfat-nofuse ];
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  # programs.mtr.enable = true;
+  # programs.gnupg.agent = {
+  #   enable = true;
+  #   enableSSHSupport = true;
+  # };
+
+  # List services that you want to enable:
+
+  # Enable the OpenSSH daemon.
+  services.openssh.enable = true;
+
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  # networking.firewall.enable = false;
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "23.11"; # Did you read the comment?
+
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+}
---
+++ nixos/flake.nix
@@ -0,0 +1,27 @@
+{
+  description = "Glorious flake";
+
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixos-unstable";
+
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, nixpkgs, ... }@inputs:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in
+    {
+      nixosConfigurations.igor = nixpkgs.lib.nixosSystem {
+        specialArgs = { inherit inputs; };
+        modules = [
+          ./configuration.nix
+          inputs.home-manager.nixosModules.default
+        ];
+      };
+    };
+}
