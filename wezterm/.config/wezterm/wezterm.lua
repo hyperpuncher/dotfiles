4745cff64c445bc7d87abaf3dc3f62d45c6c8227
hyperpuncher <39203616+hyperpuncher@users.noreply.github.com>
2023-12-29 08:58:25 +0300

    add wezterm

---
+++ wezterm/.config/wezterm/wezterm.lua
@@ -0,0 +1,47 @@
+local wezterm = require 'wezterm'
+local config = {
+
+    animation_fps = 30,
+    color_scheme = 'Monokai Soda',
+    default_cursor_style = "BlinkingBar",
+    enable_tab_bar = false,
+    font = wezterm.font 'JetBrainsMono Nerd Font',
+    font_size = 14,
+    freetype_load_flags = 'NO_HINTING',
+    front_end = "OpenGL",
+    window_close_confirmation = "NeverPrompt",
+
+    window_padding = {
+        left = '1cell',
+        right = '1cell',
+        top = '0.5cell',
+        bottom = 0,
+    },
+
+    keys = {
+        {
+            key = "c",
+            mods = "CTRL",
+            action = wezterm.action_callback(function(window, pane)
+                local has_selection = window:get_selection_text_for_pane(pane) ~= ""
+                if has_selection then
+                    window:perform_action(
+                        wezterm.action { CopyTo = "ClipboardAndPrimarySelection" },
+                        pane)
+                    window:perform_action("ClearSelection", pane)
+                else
+                    window:perform_action(
+                        wezterm.action { SendKey = { key = "c", mods = "CTRL" } },
+                        pane)
+                end
+            end)
+        },
+        {
+            key = "v",
+            mods = "CTRL",
+            action = wezterm.action { PasteFrom = "Clipboard" },
+        },
+    },
+}
+
+return config
