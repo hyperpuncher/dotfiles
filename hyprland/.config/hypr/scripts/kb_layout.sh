ee655a6c86df8b198962cb2b075d9b0053ed2be3
hyperpuncher <39203616+hyperpuncher@users.noreply.github.com>
2024-01-23 11:47:40 +0300

    cleanup

--- hyprland/.config/hypr/scripts/kb_layout.sh
+++ hyprland/.config/hypr/scripts/kb_layout.sh
@@ -10,26 +10,26 @@ en_layout_apps="code-url-handler codium-url-handler kitty firefox obsidian beeke
 ru_layout_apps="org.telegram.desktop"
 
 for name in ${kb_names}; do
-    if hyprctl devices -j | jq -e '.keyboards[].name' | grep -q "$name"; then
-        active_keyboard="$name"
-        break
-    fi
+	if hyprctl devices -j | jq -e '.keyboards[].name' | grep -q "$name"; then
+		active_keyboard="$name"
+		break
+	fi
 done
 
 while true; do
-    active_window_class=$(hyprctl activewindow | grep "class" | awk '{print $2}')
-    if [ "$active_window_class" != "$previous_window_class" ]; then
-        case " $en_layout_apps " in
-        *" $active_window_class "*)
-            hyprctl switchxkblayout "$active_keyboard" 0
-            ;;
-        esac
-        case " $ru_layout_apps " in
-        *" $active_window_class "*)
-            hyprctl switchxkblayout "$active_keyboard" 1
-            ;;
-        esac
-        previous_window_class="$active_window_class"
-    fi
-    sleep 0.2
+	active_window_class=$(hyprctl activewindow | grep "class" | awk '{print $2}')
+	if [ "$active_window_class" != "$previous_window_class" ]; then
+		case " $en_layout_apps " in
+		*" $active_window_class "*)
+			hyprctl switchxkblayout "$active_keyboard" 0
+			;;
+		esac
+		case " $ru_layout_apps " in
+		*" $active_window_class "*)
+			hyprctl switchxkblayout "$active_keyboard" 1
+			;;
+		esac
+		previous_window_class="$active_window_class"
+	fi
+	sleep 0.2
 done
