8663b2eed0cda0962077ba7fca13b7d95ab0632e
hyperpuncher <39203616+hyperpuncher@users.noreply.github.com>
2024-03-19 14:20:53 +0300

    upd wireplumber rules

--- wireplumber/.config/wireplumber/main.lua.d/51-alsa-disable.lua
+++ wireplumber/.config/wireplumber/main.lua.d/51-alsa-disable.lua
@@ -1,25 +1,25 @@
 rule = {
-    matches = {
-        -- display home
-        {
-            { "device.name", "equals", "alsa_card.pci-0000_07_00.1" },
-        },
-        {
-            { "node.name", "equals", "alsa_output.pci-0000_09_00.4.iec958-stereo" },
-        },
-        -- display work
-        {
-            { "device.name", "equals", "alsa_card.pci-0000_07_00.1.3" },
-        },
-        -- system card work
-        {
-            { "device.name", "equals", "alsa_card.pci-0000_07_00.6" },
-        }
-    },
-    apply_properties = {
-        ["node.disabled"] = true,
-        ["device.disabled"] = true,
-    },
+	matches = {
+		-- display home
+		{
+			{ "device.name", "equals", "alsa_card.pci-0000_07_00.1" },
+		},
+		{
+			{ "device.name", "equals", "alsa_output.pci-0000_09_00.4" },
+		},
+		-- display work
+		{
+			{ "device.name", "equals", "alsa_card.pci-0000_07_00.1.3" },
+		},
+		-- system card work
+		{
+			{ "device.name", "equals", "alsa_card.pci-0000_07_00.6" },
+		},
+	},
+	apply_properties = {
+		["node.disabled"] = true,
+		["device.disabled"] = true,
+	},
 }
 
 table.insert(alsa_monitor.rules, rule)
--- wireplumber/.config/wireplumber/main.lua.d/52-disable-suspend.lua
+++ wireplumber/.config/wireplumber/main.lua.d/52-disable-suspend.lua
@@ -1,53 +1,53 @@
-alsa_monitor.rules = {
-	{
-		matches = {
-			{
-				-- Matches all sources.
-				{ "node.name", "matches", "alsa_input.*" },
-			},
-			{
-				-- Matches all sinks.
-				{ "node.name", "matches", "alsa_output.*" },
-			},
+rule = {
+	matches = {
+		{
+			-- Matches all sources.
+			{ "node.name", "matches", "alsa_input.*" },
 		},
-		apply_properties = {
-			--["node.nick"]              = "My Node",
-			--["node.description"]       = "My Node Description",
-			--["priority.driver"]        = 100,
-			--["priority.session"]       = 100,
-			--["node.pause-on-idle"]     = false,
-			--["monitor.channel-volumes"] = false
-			--["resample.quality"]       = 4,
-			--["resample.disable"]       = false,
-			--["channelmix.normalize"]   = false,
-			--["channelmix.mix-lfe"]     = false,
-			--["channelmix.upmix"]       = true,
-			--["channelmix.upmix-method"] = "psd",  -- "none" or "simple"
-			--["channelmix.lfe-cutoff"]  = 150,
-			--["channelmix.fc-cutoff"]   = 12000,
-			--["channelmix.rear-delay"]  = 12.0,
-			--["channelmix.stereo-widen"] = 0.0,
-			--["channelmix.hilbert-taps"] = 0,
-			--["channelmix.disable"]     = false,
-			--["dither.noise"] = 0,
-			--["dither.method"] = "wannamaker3", -- "rectangular", "triangular" or "shaped5"
-			--["audio.channels"]         = 2,
-			--["audio.format"]           = "S16LE",
-			--["audio.rate"]             = 44100,
-			--["audio.allowed-rates"]    = "32000,96000",
-			--["audio.position"]         = "FL,FR",
-			--["api.alsa.period-size"]   = 1024,
-			--["api.alsa.period-num"]    = 2,
-			--["api.alsa.headroom"]      = 0,
-			--["api.alsa.start-delay"]   = 0,
-			--["api.alsa.disable-mmap"]  = false,
-			--["api.alsa.disable-batch"] = false,
-			--["api.alsa.use-chmap"]     = false,
-			--["api.alsa.multirate"]     = true,
-			--["latency.internal.rate"]  = 0
-			--["latency.internal.ns"]    = 0
-			--["clock.name"]             = "api.alsa.0"
-			["session.suspend-timeout-seconds"] = 0, -- 0 disables suspend
+		{
+			-- Matches all sinks.
+			{ "node.name", "matches", "alsa_output.*" },
 		},
 	},
+	apply_properties = {
+		--["node.nick"]              = "My Node",
+		--["node.description"]       = "My Node Description",
+		--["priority.driver"]        = 100,
+		--["priority.session"]       = 100,
+		--["node.pause-on-idle"]     = false,
+		--["monitor.channel-volumes"] = false
+		--["resample.quality"]       = 4,
+		--["resample.disable"]       = false,
+		--["channelmix.normalize"]   = false,
+		--["channelmix.mix-lfe"]     = false,
+		--["channelmix.upmix"]       = true,
+		--["channelmix.upmix-method"] = "psd",  -- "none" or "simple"
+		--["channelmix.lfe-cutoff"]  = 150,
+		--["channelmix.fc-cutoff"]   = 12000,
+		--["channelmix.rear-delay"]  = 12.0,
+		--["channelmix.stereo-widen"] = 0.0,
+		--["channelmix.hilbert-taps"] = 0,
+		--["channelmix.disable"]     = false,
+		--["dither.noise"] = 0,
+		--["dither.method"] = "wannamaker3", -- "rectangular", "triangular" or "shaped5"
+		--["audio.channels"]         = 2,
+		--["audio.format"]           = "S16LE",
+		--["audio.rate"]             = 44100,
+		--["audio.allowed-rates"]    = "32000,96000",
+		--["audio.position"]         = "FL,FR",
+		--["api.alsa.period-size"]   = 1024,
+		--["api.alsa.period-num"]    = 2,
+		--["api.alsa.headroom"]      = 0,
+		--["api.alsa.start-delay"]   = 0,
+		--["api.alsa.disable-mmap"]  = false,
+		--["api.alsa.disable-batch"] = false,
+		--["api.alsa.use-chmap"]     = false,
+		--["api.alsa.multirate"]     = true,
+		--["latency.internal.rate"]  = 0
+		--["latency.internal.ns"]    = 0
+		--["clock.name"]             = "api.alsa.0"
+		["session.suspend-timeout-seconds"] = 0, -- 0 disables suspend
+	},
 }
+
+table.insert(alsa_monitor.rules, rule)
