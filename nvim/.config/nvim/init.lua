00b7dd0220748004fe5cf76701598194c5dd22d6
hyperpuncher <39203616+hyperpuncher@users.noreply.github.com>
2024-09-30 13:42:54 +0300

    disable diag for ikiosuru

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -717,3 +717,10 @@ vim.filetype.add({ extension = { templ = "templ" } })
 vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, { border = "rounded" })
 vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.signature_help, { border = "rounded" })
 vim.diagnostic.config({ float = { border = "rounded" } })
+
+vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
+	pattern = "ikiosuru.py",
+	callback = function()
+		vim.diagnostic.disable()
+	end,
+})
