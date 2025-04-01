50fbc95803f63ed37cb22b34f9c7198c13d4919b
hyperpuncher <39203616+hyperpuncher@users.noreply.github.com>
2025-04-01 08:52:46 +0300

    darkman: smooth wallpaper transition

--- darkman/.local/share/dark-mode.d/dark.sh
+++ darkman/.local/share/dark-mode.d/dark.sh
@@ -8,6 +8,8 @@ kvantummanager --set ColloidDark
 sed -i 's/icon_theme=.*/icon_theme=Papirus-Dark/' ~/.config/qt6ct/qt6ct.conf
 export QT_QPA_PLATFORMTHEME=qt6ct
 
-pkill swaybg
 hyprctl dispatch exec "swaybg -m fill -i /home/igor/Pictures/wallpapers/gradient_dark.avif"
 hyprctl dispatch exec "swaybg -i /home/igor/Pictures/wallpapers/mask_vertical_4k.png"
+sleep 1
+pkill -o swaybg
+pkill -o swaybg
