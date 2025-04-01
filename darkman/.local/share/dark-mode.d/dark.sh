d43ab371f9d4d5c04755defaee02053a720766b5
hyperpuncher <39203616+hyperpuncher@users.noreply.github.com>
2025-04-01 08:21:40 +0300

    darkman: init

---
+++ darkman/.local/share/dark-mode.d/dark.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env dash
+
+gsettings set org.gnome.desktop.interface gtk-theme 'adw-gtk3-dark'
+gsettings set org.gnome.desktop.interface color-scheme 'prefer-dark'
+gsettings set org.gnome.desktop.interface icon-theme 'Papirus-Dark'
+
+kvantummanager --set ColloidDark
+sed -i 's/icon_theme=.*/icon_theme=Papirus-Dark/' ~/.config/qt6ct/qt6ct.conf
+export QT_QPA_PLATFORMTHEME=qt6ct
+
+pkill swaybg
+hyprctl dispatch exec "swaybg -m fill -i /home/igor/Pictures/wallpapers/gradient_dark.avif"
+hyprctl dispatch exec "swaybg -i /home/igor/Pictures/wallpapers/mask_vertical_4k.png"
---
+++ darkman/.local/share/light-mode.d/light.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env dash
+
+gsettings set org.gnome.desktop.interface gtk-theme 'adw-gtk3'
+gsettings set org.gnome.desktop.interface color-scheme 'prefer-light'
+gsettings set org.gnome.desktop.interface icon-theme 'Papirus-Light'
+
+kvantummanager --set Colloid
+sed -i 's/icon_theme=.*/icon_theme=Papirus/' ~/.config/qt6ct/qt6ct.conf
+export QT_QPA_PLATFORMTHEME=qt6ct
+
+pkill swaybg
+hyprctl dispatch exec "swaybg -m fill -i /home/igor/Pictures/wallpapers/gradient_light.avif"
+hyprctl dispatch exec "swaybg -i /home/igor/Pictures/wallpapers/mask_vertical_4k.png"
