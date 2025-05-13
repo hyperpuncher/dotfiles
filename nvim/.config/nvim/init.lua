31db897e11695ab4dc5837e9def5bc8a8f8585c6
hyperpuncher <39203616+hyperpuncher@users.noreply.github.com>
2025-05-13 18:12:21 +0300

    nvim: snacks file rename

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -590,3 +590,12 @@ vim.api.nvim_create_autocmd("InsertLeave", {
 		os.execute("hyprctl switchxkblayout current 0 >/dev/null 2>&1")
 	end,
 })
+
+vim.api.nvim_create_autocmd("User", {
+	pattern = "OilActionsPost",
+	callback = function(event)
+		if event.data.actions.type == "move" then
+			Snacks.rename.on_rename_file(event.data.actions.src_url, event.data.actions.dest_url)
+		end
+	end,
+})
